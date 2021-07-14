9f440af215c22855c8d4831e9270db6a303eaf4d
kkirby16 <kkirby16@my.whitworth.edu>
2021-07-13 21:00:17 -0700

    Done.

--- src/Child.js
+++ src/Child.js
@@ -1,15 +1,17 @@
-import React, { Component } from 'react'
-import { getRandomColor } from './randomColorGenerator.js'
+import React, { Component } from "react";
+import { getRandomColor } from "./randomColorGenerator.js";
 
 class Child extends Component {
   render() {
+    console.log(this.props);
     return (
       <div
+        onClick={() => this.props.handleColorChange(getRandomColor())} //why does this need to be an anonymous or arrow function?
         className="child"
-        style={{backgroundColor: "#FFF"}}
+        style={{ backgroundColor: this.props.color }}
       ></div>
-    )
+    );
   }
 }
 
-export default Child
+export default Child;
